f84b27dfdbedfe12e0dadd2c448deaba359cd229
Iterus <supermax705@gmail.com>
2016-10-16 01:24:00 +0300

    Класс 'Станок' - синглтон

--- KAMEditor/machinetool.h
+++ KAMEditor/machinetool.h
@@ -98,8 +98,21 @@ const int SEARCH_ATC_SENSOR = 0x0D;
 class MachineTool
 {
 public:
+    // всегда возвращает один и тот же экземпляр класса MachineTool
+    static MachineTool& Instance()
+    {
+        static MachineTool m;
+        return m;
+    }
 
- MachineTool();
+private:
+    // конструктор и деструктор недоступны публично
+    MachineTool();
+    ~MachineTool();
+
+    // запрет копирования
+    MachineTool(MachineTool const&);
+    MachineTool& operator=(MachineTool const&);
 };
 
 #endif // MACHINETOOL_H
