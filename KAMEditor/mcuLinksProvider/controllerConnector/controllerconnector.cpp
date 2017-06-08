2ce667ea6b8b86a2ea8e06321ca458b344e2346e
Xtail <xtail1996@yandex.ru>
2017-06-08 19:39:08 +0300

    Добавлены предварительные настройки порта

--- KAMEditor/mcuLinksProvider/controllerConnector/controllerconnector.cpp
+++ KAMEditor/mcuLinksProvider/controllerConnector/controllerconnector.cpp
@@ -13,4 +13,9 @@ void ControllerConnector::send()
 ControllerConnector::ControllerConnector()
 {
     serialPort.setBaudRate(2400);
+    serialPort.setBreakEnabled(true);
+    serialPort.setCurrentReadChannel(1);
+    serialPort.setCurrentWriteChannel(2);
+    serialPort.setReadBufferSize(1024);
+    serialPort.setPortName("U1");
 }
