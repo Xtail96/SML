1c977eabe99f8c3267dba46d1fffc179d4563c09
Xtail <xtail1996@yandex.ru>
2017-07-04 09:38:51 +0300

    Проведен рефакторинг кода

--- KAMEditor/machineTool/components/pointsManager/point/point.cpp
+++ KAMEditor/machineTool/components/pointsManager/point/point.cpp
@@ -20,7 +20,7 @@ double& Point::operator[](size_t idx)
 
 double& Point::operator[](std::string name)
 {
-    int idx = axisNames.getKeyByName(name);
+    int idx = axisesNames.getKeyByName(name);
     return operator[](idx);
 }
 
--- KAMEditor/machinetool/components/movementController/movementcontroller.cpp
+++ KAMEditor/machinetool/components/movementController/movementcontroller.cpp
@@ -1,5 +1,15 @@
 #include "movementcontroller.h"
 
+std::vector<std::shared_ptr<Axis> > MovementController::getAxises() const
+{
+    return axises;
+}
+
+void MovementController::setAxises(const std::vector<std::shared_ptr<Axis> > &value)
+{
+    axises = value;
+}
+
 MovementController::MovementController(unsigned int _axisesCount, double _step, double _velocity) :
     step(_step), velocity(_velocity), dimensionsController(_axisesCount)
 {
@@ -17,7 +27,7 @@ void MovementController::addAxises(const unsigned int &count)
     axises.reserve(axisCount);
     for(unsigned int i = 0; i < axisCount; i++)
     {
-        std::string axisName = axisNames.getNameByKey(i);
+        std::string axisName = axisesNames.getNameByKey(i);
         Axis* newAxis = new Axis(axisName);
         axises.push_back(std::shared_ptr<Axis>(newAxis));
     }
@@ -33,19 +43,19 @@ void MovementController::setupAxises()
     }
 }
 
-std::vector<std::shared_ptr<Axis> > MovementController::getAxises() const
+Point MovementController::getAxisesLength()
 {
-    return axises;
-}
-
-void MovementController::setAxises(const std::vector<std::shared_ptr<Axis> > &value)
-{
-    axises = value;
+    Point _axisesLength(axises.size());
+    for(unsigned int i = 0; i < axises.size(); i++)
+    {
+        _axisesLength[i] = axises[i]->getLength();
+    }
+    return _axisesLength;
 }
 
 void MovementController::stepMove(Point &to)
 {
-    double current_step = (step > 0) ? step : 0.01;
+    //double current_step = (step > 0) ? step : 0.01;
     if(checkCurrentCoordinates(to))
     {
         //currentCoordinatesFromBase += offset*current_step;
@@ -54,7 +64,7 @@ void MovementController::stepMove(Point &to)
 
 void MovementController::move(const Point& offset)
 {
-    double current_step = (step > 0) ? step : 0.01;
+    //double current_step = (step > 0) ? step : 0.01;
     Point newCoordinates = currentCoordinates + offset;
     if(checkCurrentCoordinates(newCoordinates))
     {
@@ -70,14 +80,4 @@ bool MovementController::checkCurrentCoordinates(Point &newCoordinates)
         isMovementCorrect = false;
     }
     return isMovementCorrect;
-}
-
-Point MovementController::getAxisesLength()
-{
-    Point _axisesLength(axises.size());
-    for(unsigned int i = 0; i < axises.size(); i++)
-    {
-        _axisesLength[i] = axises[i]->getLength();
-    }
-    return _axisesLength;
 }
