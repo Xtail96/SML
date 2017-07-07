12ba4aba9a4680f39f804bde2597e331c60e37c7
Iterus <supermax705@gmail.com>
2017-07-07 14:14:55 +0300

    Добавлена документация к UsbDevice

--- KAMEditor/machinetool/mcuLinksProvider/usbDevice/usbdevice.h
+++ KAMEditor/machinetool/mcuLinksProvider/usbDevice/usbdevice.h
@@ -6,19 +6,57 @@
 
 #include "dependencies.h"
 
+/**
+ * @brief Класс для работы с usb устройствами (чтение и запись)
+ */
 class UsbDevice
 {
 private:
+    /// Контекст, в котором происходит работа с libusb
     libusb_context *context = NULL;
+    /// Указатель на текущее устройство
     libusb_device *device = NULL;
+    /// Указатель для совершения операций ввода/вывода
     libusb_device_handle *deviceHandle = NULL;
 
+    /**
+     * @brief Инициализирует библиотеку libusb
+     * \warning Если инициализация не удалась, бросает исключение runtime_error
+     */
     void initialize_libusb();
+
+    /**
+     * @brief Инициализирует заданное устройство
+     * @param vid vendorId устройства
+     * @param pid productId устройства
+     */
     void initializeDevice(uint16_t vid, uint16_t pid);
+
+    /**
+     * @brief Осуществляет поиск заданного устройства в списке всех usb устройств
+     * @param vid vendorId устройства
+     * @param pid productId устройства
+     * @param devices список всех usb устройств
+     * @param N число всех usb устройств
+     * @return указатель на найденное устройство
+     * \warning Если устройство не было найдено, бросает исключение runtime_error
+     */
     libusb_device* findDevice(uint16_t vid, uint16_t pid, libusb_device** devices, size_t N);
+
+    /**
+     * @brief Открывает заданное устройство для чтения и записи
+     * @param device заданное устройство
+     * @return указатель для совершения операций ввода/вывода
+     * \warning Если не удалось открыть устройство, бросает исключение runtime_error
+     */
     libusb_device_handle* openDevice(libusb_device* device);
 
 public:
+    /**
+     * @brief Создает экземпляр класса для работы с заданным устройством
+     * @param vendorId
+     * @param productId
+     */
     UsbDevice(uint16_t vendorId, uint16_t productId);
     ~UsbDevice();
 };
