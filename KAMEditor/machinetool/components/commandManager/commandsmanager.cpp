5a8d7f17f874d1b026de436422316c77ac39ff84
Iterus <supermax705@gmail.com>
2017-07-13 14:42:48 +0300

    Добавлена реализация менеджера команд

--- KAMEditor/machinetool/components/commandManager/commandsmanager.cpp
+++ KAMEditor/machinetool/components/commandManager/commandsmanager.cpp
@@ -1,6 +1,61 @@
 #include "commandsmanager.h"
 
-CommandsManager::CommandsManager()
+CommandsManager::CommandsManager(std::vector<std::shared_ptr<Command> > cmds) :
+    m_commands(cmds)
 {
+}
 
+std::shared_ptr<Command>& CommandsManager::operator[](size_t idx)
+{
+    if (idx < m_commands.size())
+    {
+        return m_commands[idx];
+    }
+    else
+    {
+        std::string errMsg = "Нет команды с номером " + std::to_string(idx);
+        errMsg += " (всего " + std::to_string(m_commands.size()) + " команд)";
+
+        throw std::out_of_range(errMsg);
+    }
+}
+
+std::vector<std::shared_ptr<Command> >& CommandsManager::getCommands()
+{
+    return m_commands;
+}
+
+void CommandsManager::addCommand(Command *cmd)
+{
+    addCommand(std::shared_ptr<Command>(cmd));
+}
+
+void CommandsManager::addCommand(std::shared_ptr<Command> cmd)
+{
+    m_commands.push_back(cmd);
+}
+
+void CommandsManager::deleteCommand(Command *cmd)
+{
+    deleteCommand(std::shared_ptr<Command>(cmd));
+}
+
+void CommandsManager::deleteCommand(std::shared_ptr<Command> cmd)
+{
+    m_commands.erase( std::remove(m_commands.begin(), m_commands.end(), cmd), m_commands.end() );
+}
+
+void CommandsManager::deleteCommand(size_t idx)
+{
+    if (idx < m_commands.size())
+    {
+        m_commands.erase(m_commands.begin() + idx);
+    }
+    else
+    {
+        std::string errMsg = "Нет команды с номером " + std::to_string(idx);
+        errMsg += " (всего " + std::to_string(m_commands.size()) + " команд)";
+
+        throw std::out_of_range(errMsg);
+    }
 }
--- KAMEditor/machinetool/components/commandManager/commandsmanager.h
+++ KAMEditor/machinetool/components/commandManager/commandsmanager.h
@@ -1,11 +1,67 @@
 #ifndef COMMANDSMANAGER_H
 #define COMMANDSMANAGER_H
 
+#include <algorithm>
+#include <memory>
+#include "commands/command.h"
 
 class CommandsManager
 {
 public:
-    CommandsManager();
+    /**
+     * @brief Создает менеджер команд
+     * @param cmds вектор добавляемых команд (необязательный параметр)
+     */
+    CommandsManager(std::vector< std::shared_ptr<Command> > cmds =
+            std::vector< std::shared_ptr<Command> >());
+
+    /**
+     * @brief Возвращает команду по заданному номеру
+     * @param idx номер требуемой команды
+     * @return ссылку на команду (на указатель на команду)
+     * \warning Если idx превышает число команд, бросает исключение out_of_range
+     */
+    std::shared_ptr<Command>& operator[](size_t idx);
+
+    /**
+     * @return ссылку на список всех текущих команд
+     */
+    std::vector< std::shared_ptr<Command> >& getCommands();
+
+    /**
+     * @brief Добавляет команду в список команд
+     * @param cmd указатель на добавляемую команду
+     */
+    void addCommand(Command* cmd);
+
+    /**
+     * @brief Добавляет команду в список команд
+     * @param cmd умный указатель на добавляемую команду
+     */
+    void addCommand(std::shared_ptr<Command> cmd);
+
+    /**
+     * @brief Удаляет команду из списка команд
+     * @param cmd указатель на удаляемую команду
+     */
+    void deleteCommand(Command* cmd);
+
+    /**
+     * @brief Удаляет команду из списка команд
+     * @param cmd умный указатель на удаляемую команду
+     */
+    void deleteCommand(std::shared_ptr<Command> cmd);
+
+    /**
+     * @brief Удаляет команду из списка команд
+     * @param idx номер удаляемой команды
+     * \warning Если idx превышает число команд, бросает исключение out_of_range
+     */
+    void deleteCommand(size_t idx);
+
+private:
+    /// Список всех команд
+    std::vector< std::shared_ptr<Command> > m_commands;
 };
 
 #endif // COMMANDSMANAGER_H
