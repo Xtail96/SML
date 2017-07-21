bf01361a90abf48e2bd4b9da25a5ab353d3ddb1b
Xtail1996 <xtail1996@yandex.ru>
2017-07-21 08:20:35 +0300

    Документация к классу SiLabsU1

--- KAMEditor/models/controllerconnector/usbxpressdevicesmanager/usbxpressdevice/silabsu1.h
+++ KAMEditor/models/controllerconnector/usbxpressdevicesmanager/usbxpressdevice/silabsu1.h
@@ -3,6 +3,11 @@
 #include "models/controllerconnector/usbxpressdevicesmanager/usbxpressdevice/usbxpressdevice.h"
 #include "models/machinetool/machinetool.h"
 
+/*!
+ * \brief SiLabsU1 класс являющийся представлением контроллера u1 в системе станка
+ * Наследник UsbXpressDevice
+ * Осуществляет транзакции данных между системой и контроллером u1
+ */
 class SiLabsU1 : public UsbXpressDevice
 {
 public:
