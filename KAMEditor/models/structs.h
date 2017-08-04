b9acefac7a6e000f66e7fba7fb81de9ae61070bd
Xtail <xtail1996@yandex.ru>
2017-08-04 07:57:45 +0300

    Задокументирована структура с цветами

--- KAMEditor/models/structs.h
+++ KAMEditor/models/structs.h
@@ -260,28 +260,52 @@ public:
     }
 };
 
+/*!
+ * \brief Структура SML-цвета
+ * Возвращает цвет в формтае QString с помощью соответсвующего static метода
+ */
 struct SmlColors
 {
+    /*!
+     * \brief Возвращает зеленый цвет
+     * \return #2e8b57
+     */
     static QString green()
     {
         return QString("#2e8b57");
     }
 
+    /*!
+     * \brief Возвращает красный цвет
+     * \return #b22222
+     */
     static QString red()
     {
         return QString("#b22222");
     }
 
+    /*!
+     * \brief Возвращает серый цвет
+     * \return #333
+     */
     static QString gray()
     {
         return QString("#333");
     }
 
+    /*!
+     * \brief Возвращает белый цвет
+     * \return #fff
+     */
     static QString white()
     {
         return QString("#fff");
     }
 
+    /*!
+     * \brief Возвращает голубой цвет
+     * \return #4682b4
+     */
     static QString blue()
     {
         return QString("#4682b4");
