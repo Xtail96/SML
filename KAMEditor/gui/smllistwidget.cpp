700cab26bb5b51307c724f22f8f474667ecc8cf6
Xtail <xtail1996@yandex.ru>
2017-08-21 12:21:45 +0300

    Передвижение по списку инструментов клавишами вверх/вниз

--- KAMEditor/gui/smllistwidget.cpp
+++ KAMEditor/gui/smllistwidget.cpp
@@ -9,8 +9,61 @@ SMLListWidget::SMLListWidget(QWidget *parent) :
 void SMLListWidget::keyPressEvent(QKeyEvent *keyEvent)
 {
     QListWidgetItem *selectedItem = this->currentItem();
-    if(keyEvent->key() == Qt::Key_Return)
+    int currentRow = this->currentRow();
+    int rowsCount = this->count();
+
+    int keyPressed = keyEvent->key();
+
+    switch (keyPressed) {
+    case Qt::Key_Return:
     {
-        emit itemClicked(selectedItem);
+        if(currentRow >= 0 && currentRow < rowsCount)
+        {
+            emit itemClicked(selectedItem);
+        }
+        break;
+    }
+    case Qt::Key_Up:
+    {
+        if(currentRow > 0)
+        {
+            this->setCurrentRow(currentRow - 1);
+        }
+        else
+        {
+            if(currentRow == 0)
+            {
+                this->setCurrentRow(rowsCount - 1);
+            }
+            else
+            {
+                this->setCurrentRow(0);
+            }
+        }
+        break;
+    }
+    case Qt::Key_Down:
+    {
+        if(currentRow >= 0)
+        {
+            if(currentRow == rowsCount - 1)
+            {
+                this->setCurrentRow(0);
+            }
+            else
+            {
+                this->setCurrentRow(currentRow + 1);
+            }
+        }
+        else
+        {
+            this->setCurrentRow(rowsCount - 1);
+        }
+        break;
+    }
+    default:
+    {
+        break;
+    }
     }
 }
