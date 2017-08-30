82390ed91e6d0558ed71e9349c1ca8bbdd98432d
Xtail <xtail1996@yandex.ru>
2017-08-30 09:30:51 +0300

    Работает прогресс бар САС

--- KAMEditor/gui/options/lubricationsystemwindow.cpp
+++ KAMEditor/gui/options/lubricationsystemwindow.cpp
@@ -3,12 +3,53 @@
 
 LubricationSystemWindow::LubricationSystemWindow(QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::LubricationSystemWindow)
+    ui(new Ui::LubricationSystemWindow),
+    generalLubricationTimer(new QTimer(this)),
+    stepLubriactionTimer(new QTimer(this))
 {
     ui->setupUi(this);
+
+    connect(generalLubricationTimer, SIGNAL(timeout()), this, SLOT(stop()));
+    generalLubricationTimer->setInterval(16000);
+
+    connect(stepLubriactionTimer, SIGNAL(timeout()), this, SLOT(updateProgressBar()));
+    stepLubriactionTimer->setInterval(160);
+
+    ui->lubricateProgressBar->setValue(0);
 }
 
 LubricationSystemWindow::~LubricationSystemWindow()
 {
     delete ui;
+
+    delete generalLubricationTimer;
+
+    delete stepLubriactionTimer;
+}
+
+void LubricationSystemWindow::on_lubricatePushButton_clicked()
+{
+    // включить САС
+
+    generalLubricationTimer->start();
+    stepLubriactionTimer->start();
+
+    ui->lubricateProgressBar->setValue(0);
+}
+
+void LubricationSystemWindow::stop()
+{
+    // выключить САС
+    generalLubricationTimer->stop();
+
+    stepLubriactionTimer->stop();
+
+    ui->lubricateProgressBar->setValue(100);
+
+    ui->lastLubricationDateTimeEdit->setDate(QDate::currentDate());
+}
+
+void LubricationSystemWindow::updateProgressBar()
+{
+    ui->lubricateProgressBar->setValue(ui->lubricateProgressBar->value() + 1);
 }
--- KAMEditor/gui/options/lubricationsystemwindow.h
+++ KAMEditor/gui/options/lubricationsystemwindow.h
@@ -2,6 +2,7 @@
 #define LUBRICATIONSYSTEMWINDOW_H
 
 #include <QDialog>
+#include <QTimer>
 
 namespace Ui {
 class LubricationSystemWindow;
@@ -15,8 +16,16 @@ public:
     explicit LubricationSystemWindow(QWidget *parent = 0);
     ~LubricationSystemWindow();
 
+private slots:
+    void on_lubricatePushButton_clicked();
+    void stop();
+    void updateProgressBar();
+
 private:
     Ui::LubricationSystemWindow *ui;
+
+    QTimer* generalLubricationTimer;
+    QTimer* stepLubriactionTimer;
 };
 
 #endif // LUBRICATIONSYSTEMWINDOW_H
