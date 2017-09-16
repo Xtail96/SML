361c5dc38ab89423d12b00c226f0cf14523adef3
Xtail <xtail1996@yandex.ru>
2017-09-16 14:23:19 +0300

    Рефакторинг и документация текущего класса Контроллер главного окна

--- KAMEditor/controllers/mainwindowcontroller/mainwindowcontroller.cpp
+++ KAMEditor/controllers/mainwindowcontroller/mainwindowcontroller.cpp
@@ -2,9 +2,10 @@
 
 MainWindowController::MainWindowController(QObject *parent) : QObject(parent)
 {
+    mainBridge = new MainBridge();
+
     SettingsManager settingsManager;
     QString machineToolInformationGroupName = "MachineToolInformation";
-
     try
     {
         machineTool = new MachineTool(
@@ -20,9 +21,27 @@ MainWindowController::MainWindowController(QObject *parent) : QObject(parent)
         QMessageBox(QMessageBox::Warning, "Ошибка инициализации", QString("Ошибка инициализации станка!") + QString(e.what()) + QString("; Приложение будет закрыто.")).exec();
         exit(0);
     }
+
+#ifdef Q_OS_WIN
+    try
+    {
+        u1Manager = new UsbXpressDeviceManager(machineTool->getName());
+        emit u1IsConnected();
+    }
+    catch(std::runtime_error e)
+    {
+        u1Manager = nullptr;
+        QMessageBox(QMessageBox::Warning, "Ошибка подключения", e.what()).exec();
+        emit u1IsDisconnected();
+    }
+#endif
 }
 
 MainWindowController::~MainWindowController()
 {
+    delete mainBridge;
     delete machineTool;
+#ifdef Q_OS_WIN
+    delete u1Manager;
+#endif
 }
--- KAMEditor/controllers/mainwindowcontroller/mainwindowcontroller.h
+++ KAMEditor/controllers/mainwindowcontroller/mainwindowcontroller.h
@@ -5,19 +5,53 @@
 
 #include "models/structs.h"
 #include "models/machinetool/machinetool.h"
+#include "models/controllerconnector/usbxpressdevicesmanager/usbxpressdevicemanager.h"
 #include "controllers/mainwindowcontroller/mainbridge/mainbridge.h"
 
+/*!
+ * \brief Класс Контроллер главного окна
+ * Отвечает за всю логику работы с элементами главного окна
+ */
 class MainWindowController : public QObject
 {
     Q_OBJECT
 public:
+    /*!
+     * \brief Конструктор класса Контроллер главного окна
+     * \param parent - указатель на объект (по умолчанию равен nullptr)
+     */
     explicit MainWindowController(QObject *parent = nullptr);
+
+    /*!
+      * \brief Деструктор класса Контроллер главного окна
+      */
     ~MainWindowController();
 protected:
+    /*!
+     * \brief Указатель на станок, к которому требуется подключиться
+     */
     MachineTool* machineTool;
+#ifdef Q_OS_WIN
+    /*!
+     * \brief Указатель на класс для взаимодействия с контроллером u1 по usb
+     * \warning с помощью данного объекта, взаимодействие по usb осуществляется только в операционной системе windows
+     */
+    UsbXpressDeviceManager* u1Manager;
+#endif
+    /*!
+     * \brief Указатель на мост для связи с классом Главное окно
+     */
+    MainBridge* mainBridge;
 
 signals:
+    /// Настройки станка загружены
     void machineToolSettingsIsLoaded();
+
+    /// Контроллер u1 подключен
+    void u1Connected();
+
+    /// Контроллер u1 отключен
+    void u1IsDisconnected();
 
 public slots:
 };
