dfd53514f3ed78d3d068f188294d8db4219dadd2
Xtail <xtail1996@yandex.ru>
2018-01-19 16:41:11 +0300

    Отдельный файл со скриптами

--- KAMEditor/gui/web/ui.js
+++ KAMEditor/gui/web/ui.js
@@ -193,31 +193,3 @@ function setupGui() {
   });
 };
 
-function handleFileSelect(evt) {
-  var files = evt.target.files; // FileList object
-  //alert(evt.target);
-
-  FileIO.load(files[0], function(gcode) {
-      GCodeImporter.importText(gcode, onGCodeLoaded);
-    });
-}
-
-function readTextFile(path)
-{
-  //alert("load: " + path)
-  var rawFile = new XMLHttpRequest();
-  rawFile.open("GET", path, false);
-  rawFile.onreadystatechange = function ()
-  {
-      if(rawFile.readyState === 4)
-      {
-          if(rawFile.status === 200 || rawFile.status == 0)
-          {
-              var allText = rawFile.responseText;
-              GCodeImporter.importText(allText, onGCodeLoaded);
-          }
-      }
-  }
-  rawFile.send(null);
-}
-
