a8545512b815523f06ffebfa57aa8724ca9eaa4f
Xtail1996 <xtail1996@yandex.ru>
2019-04-14 13:31:37 +0300

    upd

--- app/SML/gui/widgets/sensors_display_widget.cpp
+++ app/SML/gui/widgets/sensors_display_widget.cpp
@@ -13,21 +13,17 @@ SensorsDisplayWidget::SensorsDisplayWidget(QWidget *parent) : QWidget(parent)
 
     m_sensorsTable = new QTableWidget(container);
 #ifdef Q_OS_MACOS
-    m_sensorsTable->setMinimumWidth(110);
-    m_sensorsTable->setMaximumWidth(110);
+    m_sensorsTable->setFixedWidth(110);
 #endif
 #ifdef Q_OS_LINUX
-    m_sensorsTable->setMinimumWidth(150);
-    m_sensorsTable->setMaximumWidth(150);
+    m_sensorsTable->setFixedWidth(150);
 #endif
 #ifdef Q_OS_WIN
-    m_sensorsTable->setMinimumWidth(150);
-    m_sensorsTable->setMaximumWidth(150);
+    m_sensorsTable->setFixedWidth(150);
 #endif
 
     m_sensorsTable->setLayoutDirection(Qt::RightToLeft);
     m_sensorsTable->setShowGrid(true);
-    //m_sensorsTable->
 
     container->layout()->addWidget(m_sensorsTable);
 
