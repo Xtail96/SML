d1a1a6525d57749840078dddf21ae2cbe15233de
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2019-09-01 22:22:59 +0300

    Более содержательный отладочный вывод автозапуска адаптеров.

--- app/SML/models/services/adapters/launcher/adapters_launcher.cpp
+++ app/SML/models/services/adapters/launcher/adapters_launcher.cpp
@@ -10,32 +10,32 @@ AdaptersLauncher::AdaptersLauncher(QObject *parent) :
 
 void AdaptersLauncher::startAdapters(QString u1AdapterPath, QString u2AdapterPath)
 {
-    qDebug() << "try to start adapters" << u1AdapterPath << u2AdapterPath;
+    qDebug() << "AdaptersLauncher::startAdapters: try to start adapters U1 =" << u1AdapterPath << "U2 =" << u2AdapterPath;
     if(!m_u1AdapterProcess->isOpen())
     {
-        qDebug() << "start u1";
+        qDebug() << "AdaptersLauncher::startAdapters: try to start u1";
         m_u1AdapterProcess->start(u1AdapterPath);
     }
 
     if(!m_u2AdapterProcess->isOpen())
     {
-        qDebug() << "start u2";
+        qDebug() << "AdaptersLauncher::startAdapters: try to start u2";
         m_u2AdapterProcess->start(u2AdapterPath);
     }
 }
 
 void AdaptersLauncher::stopAdapters()
 {
-    qDebug() << "try to stop adapters";
+    qDebug() << "AdaptersLauncher::stopAdapters: try to stop adapters";
     if(m_u1AdapterProcess->isOpen())
     {
-        qDebug() << "try to stop u1";
+        qDebug() << "AdaptersLauncher::stopAdapters: try to stop u1";
         m_u1AdapterProcess->kill();
     }
 
     if(m_u2AdapterProcess->isOpen())
     {
-        qDebug() << "try to stop u2";
+        qDebug() << "AdaptersLauncher::stopAdapters: try to stop u2";
         m_u2AdapterProcess->kill();
     }
 }
