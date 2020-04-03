8babb51d5f09d82ae118ab76d86ae3b5038e8481
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2020-04-03 05:12:29 +0300

    Инкапсуляция данных в буфере. Наружу отдается только Json

--- base_adapters/U2EchoAdapter/firmware/ArduinoU2/u2/receive_buffer.h
+++ base_adapters/U2EchoAdapter/firmware/ArduinoU2/u2/receive_buffer.h
@@ -5,18 +5,14 @@
 
 struct
 {
-public:
+private:
 	char m_data[RECIEVE_BUFFER_SIZE];
-
+	
+public:
 	void init()
 	{
 		this->clear();
 		setupSerial(9600);
-	}
-
-	void read()
-	{
-		readFromSerial(m_data);
 	}
 
 	void clear()
@@ -40,4 +36,26 @@ public:
 		}
 		return empty;
 	}
+
+	JsonObject readAsJson()
+	{
+		readFromSerial(m_data);
+		Serial.write(this->m_data);
+
+		if(this->isEmpty()) return JsonObject();
+		DynamicJsonDocument doc(RECIEVE_BUFFER_SIZE);
+		auto error = deserializeJson(doc, this->m_data);
+		if (error)
+		{
+		    Serial.print(F("deserializeJson() failed with code "));
+		    Serial.println(error.c_str());
+		    this->clear();
+		    return JsonObject();
+		}
+		
+		JsonObject root;
+		root = doc.as<JsonObject>();
+		return root;
+	}
+
 } RECIEVE_BUFFER;
--- base_adapters/U2EchoAdapter/firmware/ArduinoU2/u2/u2.ino
+++ base_adapters/U2EchoAdapter/firmware/ArduinoU2/u2/u2.ino
@@ -2,8 +2,6 @@
 #include "servo_motor_utils.h"
 #include "receive_buffer.h"
 
-JsonObject parseJsonFromBuffer();
-
 void setup()
 {
   setupMotor();
@@ -11,16 +9,16 @@ void setup()
 }
  
 void loop() {
-  RECIEVE_BUFFER.read();
-  Serial.write(RECIEVE_BUFFER.m_data);
-
-  if(!RECIEVE_BUFFER.isEmpty())
+  JsonObject root = RECIEVE_BUFFER.readAsJson();
+  
+  if(!root.isNull())
   {
-    JsonObject root = parseJsonFromBuffer();
     serializeJson(root, Serial);
   }
 
   RECIEVE_BUFFER.clear();
+  
+  
   /*if(stepsCount != 0)
   {
     // нужно пройти некоторое расстояние
@@ -46,19 +44,4 @@ void loop() {
     // Отключаем двигатель
     switchOffStepperMotor();
   }*/
-}
-
-JsonObject parseJsonFromBuffer()
-{
-  DynamicJsonDocument doc(RECIEVE_BUFFER_SIZE);
-  JsonObject root;
-  auto error = deserializeJson(doc, RECIEVE_BUFFER.m_data);
-  if (error)
-  {
-      Serial.print(F("deserializeJson() failed with code "));
-      Serial.println(error.c_str());
-      return root;
-  }
-  root = doc.as<JsonObject>(); 
-  return root;
 }
