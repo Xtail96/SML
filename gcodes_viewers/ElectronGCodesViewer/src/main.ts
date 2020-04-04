5077050cf5aac8a56b656568f3ef2d34fb6208a7
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2020-04-04 15:44:04 +0300

    working visualization

--- gcodes_viewers/ElectronGCodesViewer/src/main.ts
+++ gcodes_viewers/ElectronGCodesViewer/src/main.ts
@@ -16,7 +16,7 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  mainWindow.loadFile(path.join(__dirname, "webVisualizer", "index.html"));
 
   // Open the DevTools.
   electronLocalshortcut.register(mainWindow, 'Ctrl+Shift+I', () => { mainWindow.webContents.openDevTools(); });
@@ -37,11 +37,7 @@ app.on("ready", createWindow);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  app.quit();
 });
 
 app.on("activate", () => {
