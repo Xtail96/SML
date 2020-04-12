6c8b0dcccb12e9fd1377a938bf53d537695bdceb
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2020-04-12 16:07:47 +0300

    Вернул по умолчанию визуализатор Candle

--- app/SML/models/settings_manager/settings_manager.cpp
+++ app/SML/models/settings_manager/settings_manager.cpp
@@ -131,7 +131,8 @@ void SettingsManager::generateDefaultSettings()
         settings->setValue("U1Adapter", "/Users/xtail/Projects/SML/sml-qt/base_adapters/U1SerialAdapter/app/build-U1SerialAdapter-Desktop_Qt_5_12_8_clang_64bit-Release/U1SerialAdapter");
         settings->setValue("U2Adapter", "/Users/xtail/Projects/SML/sml-qt/base_adapters/U2EchoAdapter/app/build-U2EchoAdapter-Desktop_Qt_5_12_8_clang_64bit-Release/U2EchoAdapter");
 #ifdef Q_OS_MACX
-        settings->setValue("GCodesViewer", "/Users/xtail/Projects/SML/sml-qt/gcodes_viewers/ElectronGCodesViewer/dist/mac/ElectronGCodesViewer.app/Contents/MacOS/ElectronGCodesViewer");
+        //settings->setValue("GCodesViewer", "/Users/xtail/Projects/SML/sml-qt/gcodes_viewers/ElectronGCodesViewer/dist/mac/ElectronGCodesViewer.app/Contents/MacOS/ElectronGCodesViewer");
+        settings->setValue("GCodesViewer", "/Users/xtail/Projects/SML/sml-qt/gcodes_viewers/CandleGCodesViewer/app/build-CandleGCodesViewer-Desktop_Qt_5_12_8_clang_64bit-Release/CandleGCodesViewer");
 #else
         settings->setValue("GCodesViewer", "CandleGCodesViewer");
 #endif
