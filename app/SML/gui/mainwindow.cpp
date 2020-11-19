e70d03b3e10c679011e8c0f233d414190a57b33d
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2020-11-19 13:37:28 +0400

    upd

--- app/SML/gui/mainwindow.cpp
+++ app/SML/gui/mainwindow.cpp
@@ -16,7 +16,7 @@ MainWindow::MainWindow(QWidget *parent) :
     this->setupHardwareDriver();
     this->setupSlots();
 
-    m_adaptersLauncher->startAdapters();
+    m_adaptersLauncher->startAdapters(true, true);
 }
 
 MainWindow::~MainWindow()
